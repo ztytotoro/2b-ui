36344c0434c333cdb60c290570ddb626a321877d
Yarn <ztytotoro@outlook.com>
2018-10-19 14:55:27 +0800

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-class-decorator";
+
+@Component
+export default class Button extends Vue {
+
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import Button from "@/components/Button.vue";
+
+const components = [
+    Button
+];
