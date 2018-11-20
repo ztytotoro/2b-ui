9aab6944645350b0126fac7b2cacdf43f8b029b6
Yarn <ztytotoro@outlook.com>
2018-11-21 00:20:12 +0800

    update

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,16 +1,6 @@
 <template>
-    <div class="tb-table">
-        <div hidden>
-            <slot></slot>
-        </div>
-        <div :class="['tb-table-container', border ? 'tb-table-container--bordered' : 'tb-table-container--shadowed']">
-            <div class="tb-table-title" v-if="title">{{title}}</div>
-            <tb-table-head :heads="heads" :style="{width: width ? width : '100%'}"></tb-table-head>
-            <tb-table-body :style="{width: width ? width : '100%', height: height ? height : ''}" :class="[height && !body.hasScrollBar ? 'tb-table-body--fixed-height' : '']">
-            </tb-table-body>
-        </div>
-        <tb-row align="flex-start" padding="0">
-            <slot name="pagination"></slot>
-        </tb-row>
+    <div class="shell">
+        <div class="head"></div>
+        <div class="body"></div>
     </div>
 </template>
