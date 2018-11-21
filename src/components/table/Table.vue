462163296f140f844c93858632abf06878c34053
Yarn <ztytotoro@outlook.com>
2018-11-21 21:00:36 +0800

    update

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,6 +1,80 @@
 <template>
     <div class="shell">
-        <div class="head"></div>
-        <div class="body"></div>
+        <div class="head">
+            <div class="cell head-item"></div>
+            <div class="cell head-item"></div>
+            <div class="cell head-item"></div>
+            <div class="cell head-item"></div>
+            <div class="cell head-item"></div>
+            <div class="cell head-item"></div>
+        </div>
+        <div class="body-container">
+            <div class="body">
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+                <div class="row">
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                    <div class="cell body-item"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
